e15cb0a240f77e45a432b824b6afb22456a9d86d
t-gordzielik <tomasz.gordzielik@gmail.com>
2020-03-20 16:43:39 +0100

    Dodano wstępne połączenie z modelem

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CrownDetector.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace CrownDetector.Controllers
 {
@@ -25,15 +28,40 @@ namespace CrownDetector.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> MakePredictionRequest(HomeViewModel model)
         {
-            return View();
-        }
+            var client = new HttpClient();
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // Request headers - replace this example key with your valid Prediction-Key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "de8bf3a1c0e34c6ab6c1cb3b2a43a423");
+
+            // Prediction URL - replace this example URL with your valid Prediction URL.
+            string url = "https://crowndetector002.cognitiveservices.azure.com/customvision/v3.0/Prediction/9bf7ca7f-3df0-4a49-9088-98e7527236f0/classify/iterations/Iteration1/image";
+
+            HttpResponseMessage response;
+
+            byte[] byteData;
+
+            using (var ms = new MemoryStream())
+            {
+                model.AttachedFile.CopyTo(ms);
+                byteData = ms.ToArray();
+                //string s = Convert.ToBase64String(fileBytes);
+                // act on the Base64 data
+            }
+
+            // Request body. Try this sample with a locally stored image.
+            
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                model.Result = await response.Content.ReadAsStringAsync();
+            }
+
+            return View("Index", model);
         }
     }
 }
